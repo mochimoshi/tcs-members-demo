1e78752fc3402741b1de6937bd33c38051a4a34f
Alex Wang <alex.wang@chromatiqa.org>
2014-10-09 21:20:04 -0700

    Initial Commit

---
+++ TCSMembers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCSMembers
+//
+//  Created by Alex Yuh-Rern Wang on 10/9/14.
+//  Copyright (c) 2014 Chromatiqa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCSMembers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TCSMembers
+//
+//  Created by Alex Yuh-Rern Wang on 10/9/14.
+//  Copyright (c) 2014 Chromatiqa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
